0b729c6cf1fb73a2932bc8436443aab82201d94d
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2017-03-16 10:32:33 +0200

    Updated ServiceProvider to fit HCBaseServiceProvider

--- src/app/providers/HCAppsServiceProvider.php
+++ src/app/providers/HCAppsServiceProvider.php
@@ -2,113 +2,27 @@
 
 namespace interactivesolutions\honeycombapps\app\providers;
 
-use interactivesolutions\honeycombapps\app\http\middleware\AuthApps;
 use Illuminate\Routing\Router;
-use Illuminate\Support\ServiceProvider;
+use interactivesolutions\honeycombapps\app\http\middleware\AuthApps;
+use interactivesolutions\honeycombcore\providers\HCBaseServiceProvider;
 
-class HCAppsServiceProvider extends ServiceProvider
+class HCAppsServiceProvider extends HCBaseServiceProvider
 {
     /**
      * Register commands
      *
      * @var array
      */
-    protected $commands = [
-    ];
+    protected $commands = [];
 
     protected $namespace = 'interactivesolutions\honeycombapps\app\http\controllers';
 
     /**
-     * Bootstrap the application services.
+     * @param Router $router
      */
-    public function boot(Router $router)
-    {
-        // register artisan commands
-        $this->commands($this->commands);
-
-        // loading views
-        $this->loadViewsFrom(__DIR__ . '/../../resources/views', 'HCApps');
-
-        // loading translations
-        $this->loadTranslationsFrom(__DIR__ . '/../../resources/lang', 'HCApps');
-
-        // registering elements to publish
-        $this->registerPublishElements();
-
-        // registering helpers
-        $this->registerHelpers();
-
-        // registering routes
-        $this->registerRoutes();
-
-        //register providers
-        $this->registerProviders();
-
-        //register middleware
-        $this->registerMiddleware($router);
-    }
-
-    /**
-     * Register helper function
-     */
-    private function registerHelpers()
-    {
-        $filePath = __DIR__ . '/../http/helpers.php';
-
-        if (file_exists($filePath))
-            require_once $filePath;
-    }
-
-    /**
-     *  Registering all vendor items which needs to be published
-     */
-    private function registerPublishElements ()
-    {
-        $directory = __DIR__ . '/../../database/migrations/';
-
-        // Publish your migrations
-        if (file_exists ($directory))
-            $this->publishes ([
-                __DIR__ . '/../../database/migrations/' => database_path ('/migrations'),
-            ], 'migrations');
-
-        $directory = __DIR__ . '/../public';
-
-        // Publishing assets
-        if (file_exists ($directory))
-            $this->publishes ([
-                __DIR__ . '/../public' => public_path ('honeycomb'),
-            ], 'public');
-    }
-
-    /**
-     * @param $gate
-     * @param $router
-     */
-    private function registerMiddleware (Router $router)
+    protected function registerMiddleware (Router $router)
     {
         $router->middleware ('auth-apps', AuthApps::class);
-    }
-
-    /**
-     * Registering routes
-     */
-    private function registerRoutes()
-    {
-        $filePath = __DIR__ . '/../../app/honeycomb/routes.php';
-
-        if (file_exists($filePath))
-            \Route::group (['namespace' => $this->namespace], function ($router) use ($filePath) {
-                require $filePath;
-            });
-    }
-
-    /**
-     * Registering 3rd party providers which are required for this package to run
-     */
-    private function registerProviders()
-    {
-
     }
 }
 
