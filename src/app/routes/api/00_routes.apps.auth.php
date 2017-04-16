19148ecb9815e5e495201ae3799c9b4630e18938
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2017-04-16 21:09:36 +0300

    Removed old version of apps

--- src/app/routes/api/00_routes.apps.auth.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-Route::group(['prefix' => 'api', 'middleware' => ['web']], function () {
-    Route::get('login', ['as' => 'api.v1.api.login', 'uses' => 'HCAppsController@showLogin']);
-    Route::post('login', ['as' => 'api.v1.api.login', 'uses' => 'HCAppsController@login']);
-
-    Route::get ('logout', ['as' => 'api.v1.api.logout', 'middleware' => 'auth-apps', 'uses' => 'HCAppsController@logout']);
-
-});
