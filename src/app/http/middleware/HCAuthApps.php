8b4ff5d9c1f7f6d1377d50b825d1f6fe8e3e95f8
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2017-04-16 22:45:03 +0300

    Added auth-apps middleware

---
+++ src/app/http/middleware/HCAuthApps.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace interactivesolutions\honeycombapps\app\http\middleware;
+
+use Carbon\Carbon;
+use HCLog;
+use Closure;
+use interactivesolutions\honeycombapps\app\models\apps\HCAppsTokens;
+
+class HCAuthApps
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (!$request->token)
+            return HCLog::error('APP-001', trans(trans ("HCApps::apps_tokens.missing")));
+
+        $record = HCAppsTokens::where('token', $request->token)->where('expires_at', '>=', Carbon::now())->first();
+
+        if (!$record)
+            return HCLog::error('APP-002', trans ("HCApps::apps_tokens.not_found_or_expired"));
+
+        //TODO add this value in hc:env command
+        $extendLifeSpan = env('HC_API_TOKEN_LIFESPAN_EXPAND', 0);
+
+        if ($extendLifeSpan > 0)
+        {
+            $expire_max = Carbon::now()->addMinutes($extendLifeSpan);
+            $expire_at = new Carbon($record->expires_at);
+
+            if ($expire_at < $expire_max)
+                $record->update(['expires_at' => $expire_max]);
+        }
+
+        return $next($request);
+    }
+}
--- src/app/providers/HCAppsServiceProvider.php
+++ src/app/providers/HCAppsServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace interactivesolutions\honeycombapps\app\providers;
 
+use Illuminate\Routing\Router;
+use interactivesolutions\honeycombapps\app\http\middleware\HCAuthApps;
 use interactivesolutions\honeycombcore\providers\HCBaseServiceProvider;
 
 class HCAppsServiceProvider extends HCBaseServiceProvider
@@ -13,6 +15,11 @@ class HCAppsServiceProvider extends HCBaseServiceProvider
     protected $namespace = 'interactivesolutions\honeycombapps\app\http\controllers';
 
     public $serviceProviderNameSpace = 'HCApps';
+
+    protected function registerMiddleWare (Router $router)
+    {
+        $router->middleware('auth-apps', HCAuthApps::class);
+    }
 }
 
 
