6c947dabddb12f7f0f50f7c3eef4593caf1ae61a
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2017-04-16 21:11:55 +0300

    Initial package commit

---
+++ src/app/providers/HCAppsServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace interactivesolutions\honeycombapps\app\providers;
+
+use interactivesolutions\honeycombcore\providers\HCBaseServiceProvider;
+
+class HCAppsServiceProvider extends HCBaseServiceProvider
+{
+    protected $homeDirectory = __DIR__;
+
+    protected $commands = [];
+
+    protected $namespace = 'interactivesolutions\honeycombapps\app\http\controllers';
+
+    public $serviceProviderNameSpace = '{serviceProviderNameSpace}';
+}
+
+
+
+
+
